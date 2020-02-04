30b45affa2236d2d23ea0e5ac0e171477feae26e
wtian-caredx <wtian@caredx.com>
2020-02-03 23:50:25 -0800

    Code to describe and show a strategy for imputing missing data

--- plot.R
+++ plot.R
@@ -25,10 +25,23 @@ median_steps_per_day <- median(steps_per_day)
 #What is the average daily activity pattern?
 #Time series plot of the average number of steps taken
 steps_per_interval <- tapply(data$steps, data$interval, FUN=mean, na.rm=TRUE)
-plot(steps_per_interval, type='l', xlab='5 min interval')
+steps_per_interval <- data.frame(key=names(steps_per_interval), value=steps_per_interval)
+plot(steps_per_interval$key, steps_per_interval$value, type='l', xlab='5 min interval')
+names(steps_per_interval) <- c('interval', 'steps')
 
 #Which 5-minute interval, on average across all the days in the dataset, 
 #contains the maximum number of steps?
-max_interval <- which.max(steps_per_interval)
+max_idx <- which.max(steps_per_interval$steps)
+max_interval <- steps_per_interval[max_idx,]$interval
 
 #Imputing missing values
+total_missing_value <- table(is.na(data$steps))['TRUE']
+
+#Devise a strategy for filling in all of the missing values in the dataset. 
+#For example, you could use the mean/median for that day, or the mean for that
+#5-minute interval, etc.
+new_data<- data
+for(i in as.numeric(names(table(data$interval)))){
+    new_data[new_data$interval==i,]$steps <- steps_per_interval[steps_per_interval$interval==i,]$steps
+}
+
